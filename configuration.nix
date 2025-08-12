e48cd4d55ae03a1d973a124f86cc6b42b4bf622f
Millan Kumar (nixos server) <millankumar@gmail.com>
2025-08-12 15:01:49 -0700

    switching to flakes?

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,10 @@
 { config, pkgs, ... }:
 
 let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+  home-manager = builtins.fetchTarball {
+    url = https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+    sha256 = "026rvynmzmpigax9f8gy9z67lsl6dhzv2p6s8wz4w06v3gjvspm1";
+  };
 in
 {
   imports =
@@ -13,6 +16,8 @@ in
       ./hardware-configuration.nix
       (import "${home-manager}/nixos")
     ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
